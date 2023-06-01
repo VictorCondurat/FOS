83be22827ab51f185d023581c1ff0f24be3c63f3
George Cojocaru <geocaru@gmail.com>
2023-06-01 22:09:13 +0300

    try-clear-cookie

--- src/views/scripts/index.ts
+++ src/views/scripts/index.ts
@@ -6,60 +6,60 @@ if (mobileMenuToggle) {
     mobileMenuToggle.addEventListener('click', toggleMobileMenu);
 }
 
-// Check user's login status and update the buttons accordingly
-window.addEventListener('DOMContentLoaded', () => {
-    console.log("Something");
-    const loggedIn = checkUserLoginStatus(); // Function to check user's login status
+// // Check user's login status and update the buttons accordingly
+// window.addEventListener('DOMContentLoaded', () => {
+//     console.log("Something");
+//     const loggedIn = checkUserLoginStatus(); // Function to check user's login status
 
-    if (loggedIn) {
-        // User is logged in, hide login and sign-up buttons, and display logout button
-        const loginButton = document.querySelector('.header__navbar-button--login') as HTMLButtonElement;
-        const signUpButton = document.querySelector('.header__navbar-button--signup') as HTMLButtonElement;
-        console.log("Logged in");
-        if (loginButton && signUpButton && signUpButton.parentNode) {
-            const logoutButton = document.createElement('button');
-            logoutButton.className = 'header__navbar-button header__navbar-button--logout';
-            logoutButton.textContent = 'Logout';
-            logoutButton.addEventListener('click', logout);
+//     if (loggedIn) {
+//         // User is logged in, hide login and sign-up buttons, and display logout button
+//         const loginButton = document.querySelector('.header__navbar-button--login') as HTMLButtonElement;
+//         const signUpButton = document.querySelector('.header__navbar-button--signup') as HTMLButtonElement;
+//         console.log("Logged in");
+//         if (loginButton && signUpButton && signUpButton.parentNode) {
+//             const logoutButton = document.createElement('button');
+//             logoutButton.className = 'header__navbar-button header__navbar-button--logout';
+//             logoutButton.textContent = 'Logout';
+//             logoutButton.addEventListener('click', logout);
 
-            signUpButton.parentNode.insertBefore(logoutButton, signUpButton.nextSibling);
-            loginButton.style.display = 'none';
-            signUpButton.style.display = 'none';
-        }
-    }
-});
+//             signUpButton.parentNode.insertBefore(logoutButton, signUpButton.nextSibling);
+//             loginButton.style.display = 'none';
+//             signUpButton.style.display = 'none';
+//         }
+//     }
+// });
 
-// Helper function to check the user's login status
-function checkUserLoginStatus() {
-    // Retrieve the value of the session cookie
-    const sessionCookie = getSessionCookie();
-    console.log(sessionCookie);
-    // Check if the session cookie exists
-    return sessionCookie !== '';
-}
+// // Helper function to check the user's login status
+// function checkUserLoginStatus() {
+//     // Retrieve the value of the session cookie
+//     const sessionCookie = getSessionCookie();
+//     console.log(sessionCookie);
+//     // Check if the session cookie exists
+//     return sessionCookie !== '';
+// }
 
-// Helper function to retrieve the value of the session cookie
-function getSessionCookie() {
-    const cookieString = document.cookie;
-    const cookies = cookieString.split(';');
+// // Helper function to retrieve the value of the session cookie
+// function getSessionCookie() {
+//     const cookieString = document.cookie;
+//     const cookies = cookieString.split(';');
 
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
+//     for (let i = 0; i < cookies.length; i++) {
+//         const cookie = cookies[i].trim();
 
-        // Check if the cookie starts with "session="
-        if (cookie.startsWith('session=')) {
-            // Return the value of the session cookie
-            return cookie.substring('session='.length);
-        }
-    }
+//         // Check if the cookie starts with "session="
+//         if (cookie.startsWith('session=')) {
+//             // Return the value of the session cookie
+//             return cookie.substring('session='.length);
+//         }
+//     }
 
-    return ''; // Session cookie not found
-}
+//     return ''; // Session cookie not found
+// }
 
-function logout() {
-    // Clear the session cookie by setting an expired date
-    document.cookie = 'session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+// function logout() {
+//     // Clear the session cookie by setting an expired date
+//     document.cookie = 'session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
-    // Redirect the user to the login page or any other desired page
-    window.location.href = '/login.html'; // Redirect to the login page
-}
+//     // Redirect the user to the login page or any other desired page
+//     window.location.href = '/login.html'; // Redirect to the login page
+// }
